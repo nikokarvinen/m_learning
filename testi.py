16c35f0f2dc8ab9c6b3a16c39dbb85b580995eca
AB8939 <AB8939@student.jamk.fi>
2023-02-28 22:01:13 +0200

    added log regression

---
+++ testi.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from enum import auto
+
+
+
+import numpy as np
+import pandas as pd
+
+rivi = np.array = [1,2]
+
+def laskesumma(summa):
+    rivi
+    
+
+print('hi')
+
+
+
